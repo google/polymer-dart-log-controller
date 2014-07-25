f7f736bb17c71f4cbe2b87118e29b551fdcfae77
Jacob MacDonald <jakemac@google.com>
2014-07-25 10:09:12 -0700

    code review updates

--- lib/log_controller.dart
+++ lib/log_controller.dart
@@ -8,7 +8,11 @@ import 'package:logging/logging.dart';
 @CustomTag('log-controller')
 class LogController extends PolymerElement {
   // A string representation of the current logging level.
-  @published String logLevel = (() => Logger.root.level.name)();
+  // TODO(jakemac): Once available switch to the new property pattern:
+  //   @published
+  //   String get logLevel => readValue(#logLevel);
+  //   set logLevel(String value) => writeValue(#logLevel, value.toUpperCase());
+  @published String logLevel = Logger.root.level.name;
 
   // Whether or not this should automatically pass all logs through to the
   // console.
@@ -17,25 +21,20 @@ class LogController extends PolymerElement {
   // Static listener on the root logger, null if printToConsole == false.
   static StreamSubscription _logListener;
   // Names for all the valid logging levels.
-  static List<String> allLevelNames = () {
-    return new List<String>.from(Level.LEVELS.map((level) => level.name));
+  static final Map<String, Level> _ALL_LEVELS = () {
+    var map = {};
+    Level.LEVELS.forEach((l) { map[l.name] = l; });
+    return map;
   }();
 
   LogController.created() : super.created() {
-    setListeningState();
+    updateListenter();
   }
 
-  List<String> get levelNames => LogController.allLevelNames;
+  Iterable<String> get levelNames => LogController._ALL_LEVELS.keys;
 
   // Listens to logLevel changes and updates the root logger accordingly.
   void logLevelChanged(String oldLogLevel, String newLogLevel) {
-    // Force the uppercase version of the level names.
-    var upperNewLogLevel = newLogLevel.toUpperCase();
-    if (newLogLevel != upperNewLogLevel) {
-      this.logLevel = upperNewLogLevel;
-      return;
-    }
-
     // Set the level using its name, and close the panel.
     setLevelByName(newLogLevel);
     if (this.$['collapseLevels'].opened) {
@@ -44,24 +43,22 @@ class LogController extends PolymerElement {
   }
 
   // Any time printToConsole changes, make sure _logListener is updated.
-  void printToConsoleChanged() => setListeningState();
+  void printToConsoleChanged() => updateListenter();
 
   void setLevelByName(String name) {
-    var level = Level.LEVELS.firstWhere(
-        (level) { return level.name == name;},
-        orElse: () {
-          _logger.warning("log level $name not found");
-          return null;
-        }
-    );
-    if (level != null) Logger.root.level = level;
+    var level = _ALL_LEVELS[name];
+    if (level == null) {
+      _logger.warning("log level $name not found");
+      return;
+    }
+    Logger.root.level = level;
   }
 
   void toggleCollapseLevels() => $['collapseLevels'].toggle();
 
   // Starts/Stops listening to the logger and printing to the console, depending
   // on the printToConsole setting.
-  void setListeningState() {
+  void updateListenter() {
     if (_logListener == null && printToConsole) {
       _logListener = Logger.root.onRecord.listen((rec) {
         print(rec.message);
